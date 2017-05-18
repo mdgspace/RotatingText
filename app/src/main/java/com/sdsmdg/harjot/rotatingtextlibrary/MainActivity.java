2f27c716103a311e80f1a8831a7ddc298527a083
harjot-oberai <harjot.oberai@gmail.com>
2017-05-18 12:06:38 +0530

    Updates sample code

--- app/src/main/java/com/sdsmdg/harjot/rotatingtextlibrary/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/rotatingtextlibrary/MainActivity.java
@@ -2,12 +2,8 @@ package com.sdsmdg.harjot.rotatingtextlibrary;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.BounceInterpolator;
 import android.view.animation.DecelerateInterpolator;
 
 import com.sdsmdg.harjot.rotatingtext.RotatingTextWrapper;
@@ -15,8 +11,8 @@ import com.sdsmdg.harjot.rotatingtext.models.Rotatable;
 
 public class MainActivity extends AppCompatActivity {
 
-    RotatingTextWrapper rotatingTextWrapper, rotatingTextWrapper1, rotatingTextWrapper2;
-    Rotatable rotatable, rotatable2;
+    RotatingTextWrapper rotatingTextWrapper;
+    Rotatable rotatable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
         rotatingTextWrapper = (RotatingTextWrapper) findViewById(R.id.custom_switcher);
         rotatingTextWrapper.setTypeface(typeface);
-        rotatingTextWrapper.setSize(20);
+        rotatingTextWrapper.setSize(30);
 
-        rotatable = new Rotatable(Color.parseColor("#000000"), 1000, "Word", "Word02", "Word03");
+        rotatable = new Rotatable(Color.parseColor("#FFA036"), 1000, "Word", "Word02", "Word03");
         rotatable.setTypeface(typeface);
-        rotatable.setSize(20);
+        rotatable.setSize(30);
         rotatable.setAnimationDuration(500);
-        rotatable.setCenter(true);
         rotatable.setInterpolator(new DecelerateInterpolator());
 
-        rotatable2 = new Rotatable(Color.parseColor("#FFA036"), 1000, "qwerty", "qwerty04", "qwerty06", "qwerty08");
-        rotatable2.setTypeface(typeface);
-        rotatable2.setSize(20);
-        rotatable2.setAnimationDuration(500);
-        rotatable2.setInterpolator(new DecelerateInterpolator());
-
-        rotatingTextWrapper.setContent("The words are ? and ?", rotatable, rotatable2);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                rotatingTextWrapper.pause(0);
-            }
-        }, 4500);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                rotatingTextWrapper.resume(0);
-            }
-        }, 8500);
+        rotatingTextWrapper.setContent("This is ?", rotatable);
     }
 }
