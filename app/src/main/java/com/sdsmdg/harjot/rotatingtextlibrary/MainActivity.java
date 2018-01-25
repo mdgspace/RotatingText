824ac9852bd9011d9cd7b165495902c08f50f089
harjot-oberai <harjot.oberai@gmail.com>
2018-01-25 17:59:38 +0530

    Update gradle

--- app/src/main/java/com/sdsmdg/harjot/rotatingtextlibrary/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/rotatingtextlibrary/MainActivity.java
@@ -2,9 +2,11 @@ package com.sdsmdg.harjot.rotatingtextlibrary;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
 
 import com.sdsmdg.harjot.rotatingtext.RotatingTextWrapper;
 import com.sdsmdg.harjot.rotatingtext.models.Rotatable;
@@ -12,7 +14,7 @@ import com.sdsmdg.harjot.rotatingtext.models.Rotatable;
 public class MainActivity extends AppCompatActivity {
 
     RotatingTextWrapper rotatingTextWrapper;
-    Rotatable rotatable;
+    Rotatable rotatable, rotatable2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +22,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Raleway-Light.ttf");
+        Typeface typeface2 = Typeface.createFromAsset(getAssets(), "fonts/Reckoner_Bold.ttf");
 
-        rotatingTextWrapper = (RotatingTextWrapper) findViewById(R.id.custom_switcher);
-        rotatingTextWrapper.setTypeface(typeface);
+        rotatingTextWrapper = findViewById(R.id.custom_switcher);
         rotatingTextWrapper.setSize(30);
+        rotatingTextWrapper.setTypeface(typeface2);
 
-        rotatable = new Rotatable(Color.parseColor("#FFA036"), 1000, "Word", "Word02", "Word03");
+        rotatable = new Rotatable(Color.parseColor("#FFA036"), 1000, "Word00", "Word01", "Word02");
+        rotatable.setSize(25);
         rotatable.setTypeface(typeface);
-        rotatable.setSize(30);
+        rotatable.setInterpolator(new AccelerateInterpolator());
         rotatable.setAnimationDuration(500);
-        rotatable.setInterpolator(new DecelerateInterpolator());
 
-        rotatingTextWrapper.setContent("This is ?", rotatable);
+        rotatable2 = new Rotatable(Color.parseColor("#123456"), 1000, "Word03", "Word04", "Word05");
+        rotatable2.setSize(25);
+        rotatable2.setTypeface(typeface);
+        rotatable2.setInterpolator(new DecelerateInterpolator());
+        rotatable2.setAnimationDuration(500);
+
+        rotatingTextWrapper.setContent("? ?", rotatable, rotatable2);
+
     }
 }
